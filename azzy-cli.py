70c8834907255cea64d81d3237aca86bb254a704
arsalan <arsalan.dev.engineer@gmail.com>
2024-07-06 16:11:08 +0100

    added comments to azzy-cli.py script

--- azzy-cli.py
+++ azzy-cli.py
@@ -1,19 +1,44 @@
 #!/usr/bin/env python3
-import click
-from commands import todo, greetings, joke, youtube, test, calculator
 
+# import click library
+import click
+
+# import modules from commands directory
+from commands import todo
+from commands import greetings
+from commands import joke
+from commands import youtube
+from commands import test
+from commands import calculator
+
+
+# define main command group for the CLI Tool
 @click.group(help="Azzy's CLI tool:A command-line interface for various utilities.")
 def cli():
     """Main entry point for Azzy's CLI Tool."""
+     # function doesn't do anything.
+     # is REQUIRED for defining the command group
     pass
 
-# Adds command groups to the cli group
+
+"""
+Adds command groups to the `cli` group. 
+Each command group is a separate subcommand namespace.
+The first part is the module and the second part specifies the command to be added.
+"""
+
+# example
+# adding the 'calculator' command group to the CLI
+# `calculator.calculator` refers to the `calculator` module's `calculator` command group
+cli.add_command(calculator.calculator, name="calculator")
 cli.add_command(todo.todo, name='todo')
 cli.add_command(greetings.greetings)
 cli.add_command(joke.joke, name='joke')
 cli.add_command(youtube.youtube, name='youtube')
 cli.add_command(test.test, name='test')
-cli.add_command(calculator.calculator, name="calculator")
 
+
+# Entry point of the script.
+# Calls the CLI tool if the script is executed.
 if __name__ == "__main__":
     cli()
