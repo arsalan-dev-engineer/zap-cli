bf797965b67d6c3babb01151846a9b18f2bf077b
Arsalan Arref <arsalan.arref@version1.com>
2024-12-02 01:20:59 +0000

    pushing changes

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ from setuptools import setup, find_packages
 """
 setup.py fo dev-cli
 
-This script is used to package and distribute the azzy-cli project.
+This script is used to package and distribute the zap-cli project.
 It includes details like the package name, version, dependencies, 
 and entry points, making it easy to install and use as a command-line tool.
 
 Key features of this setup.py file:
 - Defines the package name and version.
 - Lists required dependencies (like the Click library).
-- Specifies the command-line entry point, allowing the 'azzy-cli' command to be used in the terminal.
+- Specifies the command-line entry point, allowing the 'zap-cli' command to be used in the terminal.
 - Enables installation in development mode (editable mode) with `pip install -e .`.
 
 Running `python setup.py install` or `pip install -e .` allows users to install 
-the azzy-cli package and make the azzy-cli command available globally.
+the zap-cli package and make the zap-cli command available globally.
 
 This file should be included in version control (e.g., Git) since it is essential for 
 package installation and distribution, but the generated `*.egg-info/` directories 
@@ -22,7 +22,7 @@ should be excluded from version control.
 """
 
 setup(
-    name='azzy-cli',
+    name='zap-cli',
     version='0.1',
     packages=find_packages(),
     include_package_data=True,
@@ -31,7 +31,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'azzy-cli=azzy_cli:cli',
+            'zap-cli=zap_cli:cli',
         ],
     },
     classifiers=[
